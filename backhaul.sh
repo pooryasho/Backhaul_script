507f34b295d21cdcb7f013fe764d78630cd5b39d
pooryasho <142620046+pooryasho@users.noreply.github.com>
2025-01-27 15:27:24 +0330

    backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo2.sh https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/logo.sh
+wget -O /etc/logo2.sh https://raw.githubusercontent.com/pooryasho/Backhaul_script/refs/heads/main/logo.sh
 chmod +x /etc/logo2.sh
 if [ -f "backhaul.py" ]; then
     rm backhaul.py
 fi
-wget https://github.com/Azumi67/Backhaul_script/releases/download/script/backhaul.py
+wget https://github.com/pooryasho/Backhaul_script/releases/download/script/backhaul.py
 python3 backhaul.py
