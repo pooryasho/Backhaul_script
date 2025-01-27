bada0855a1502d230daf6b43a8c7b905ec3c8497
pooryasho <142620046+pooryasho@users.noreply.github.com>
2025-01-27 16:09:47 +0330

    backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -6,5 +6,9 @@ chmod +x /etc/logo2.sh
 if [ -f "backhaul.py" ]; then
     rm backhaul.py
 fi
+apt install python3 -y && sudo apt install python3-pip &&  pip install colorama && pip install netifaces && apt install curl -y
+pip3 install colorama
+sudo apt-get install python-pip -y  &&  apt-get install python3 -y && alias python=python3 && python -m pip install colorama && python -m pip install netifaces
+sudo apt update -y && sudo apt install -y python3 python3-pip curl && pip3 install --upgrade pip && pip3 install netifaces colorama requests
 wget https://github.com/pooryasho/Backhaul_script/blob/main/backhaul.py
 python3 backhaul.py
