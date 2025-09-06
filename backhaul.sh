eac6c0361e27afdf7066744be59af141a69fc4be
pooryasho <142620046+pooryasho@users.noreply.github.com>
2025-09-07 01:44:18 +0330

    backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -5,5 +5,5 @@ chmod +x /etc/logo2.sh
 if [ -f "backhaul.py" ]; then
     rm backhaul.py
 fi
-wget https://github.com/pooryasho/Backhaul_script/releases/download/v0.7.2/backhaul.py
+wget https://github.com/pooryasho/Backhaul_script/releases/download/0.7.2/backhaul.py
 python3 backhaul.py
